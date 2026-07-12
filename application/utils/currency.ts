b5f96fdfe2a6cd659a62d774074627facff4a452
Sajad Ghiasvand <smjmtsmg@gmail.com>
2026-07-13 03:25:15 +0330

    final

--- application/utils/currency.ts
+++ application/utils/currency.ts
@@ -4,6 +4,9 @@ export const formatNumberWithSeparator = (num: number): string => {
   return num?.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
 };
 
+export const formatPrice = (num: number): string =>
+  `${formatNumberWithSeparator(num)} ${CURRENCY_LABEL}`;
+
 export const formatAmountInput = (text: string): string => {
   const cleanText = text.replace(/[^\d.]/g, '');
   const parts = cleanText.split('.');
